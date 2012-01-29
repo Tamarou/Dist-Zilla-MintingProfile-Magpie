d798d9f2932ffdc3fd25519a729b855c2af6aeed
Chris Prather <chris@prather.org>
2012-01-28 23:30:10 -0500

    add a simple profile based on teh Tamarou.com website

--- lib/Dist/Zilla/MintingProfile/Magpie.pm
+++ lib/Dist/Zilla/MintingProfile/Magpie.pm
@@ -1,9 +1,10 @@
-package Dist-Zilla-MintingProfile-Magpie;
+package Dist::Zilla::MintingProfile::Magpie;
 use Moose;
 use namespace::autoclean;
 
-# ABSTRACT: My New App!
+# ABSTRACT: A Dist::Zilla Minting Profile for New Magpie Apps
 
+with 'Dist::Zilla::Role::MintingProfile::ShareDir';
 
 __PACKAGE__->meta->make_immutable;
 1;
---
+++ profiles/default/app/app.psgi
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib 'lib';
+use {{$dist->name}};
+
+{{$dist->name}}->new(
+
+)->app;
